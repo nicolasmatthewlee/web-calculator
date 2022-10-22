d67a30a1fca324db235f0aedcec1a183731f5759
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2022-10-21 21:05:12 -0700

    improved implementation of button layout

--- scripts.js
+++ scripts.js
@@ -10,26 +10,24 @@ calculator_container.appendChild(view);
 const grid = document.createElement('div');
 grid.style.backgroundColor = 'red';
 grid.style.grid = 'repeat(4,50px) / repeat(5,50px)';
+grid.style.rowGap='10px';
+grid.style.columnGap='10px';
 grid.style.display='grid';
-grid.style.width='500px';
-grid.style.height='500px';
 calculator_container.appendChild(grid);
 
 for (let i=1;i<20;i++) {
 
-    let height='50px';
-    let span='span 1';
-        
-    if (i==14) {
-        height='100px';
-        span='span 2';
-    }
-
     const button = document.createElement('button');
     button.style.width='50px';
-    button.style.height=height;
-    button.style.gridRow=span;
     button.textContent=i;
+
+    if (i==14) {
+        button.style.height='100px';
+        button.style.gridRow='span 2';
+    } else {
+        button.style.height='50px';
+    }
+
     grid.appendChild(button);
 
 }
