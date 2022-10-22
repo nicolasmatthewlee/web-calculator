50b37fb5793666149e47d5e46b93bf7a8f77d4e2
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2022-10-21 21:31:05 -0700

    centered calculator

--- scripts.js
+++ scripts.js
@@ -1,31 +1,48 @@
+document.body.style.display='flex';
+document.body.style.justifyContent='center';
+document.body.style.backgroundColor='blue';
+document.body.style.height='100vh';
+document.body.style.margin=0;
+
 const calculator_container = document.createElement('div');
-calculator_container.style.backgroundColor = 'blue';
+//calculator_container.style.height='100vh';
+calculator_container.style.display='flex';
+calculator_container.style.flexDirection='column';
+calculator_container.style.justifyContent='center';
 document.body.appendChild(calculator_container);
 
 const view = document.createElement('div');
 view.style.backgroundColor = 'green';
+view.style.height='100px';
+view.style.fontSize='70px';
+view.style.textAlign='right';
+
 view.textContent='000000000';
 calculator_container.appendChild(view);
 
+let button_width=80;
+let button_height=60;
+let gap=10;
+
 const grid = document.createElement('div');
 grid.style.backgroundColor = 'red';
-grid.style.grid = 'repeat(4,50px) / repeat(5,50px)';
-grid.style.rowGap='10px';
-grid.style.columnGap='10px';
+grid.style.grid = `repeat(4,${button_height+'px'}) / repeat(5,${button_width+'px'})`;
+grid.style.rowGap=gap+'px';
+grid.style.columnGap=gap+'px';
 grid.style.display='grid';
 calculator_container.appendChild(grid);
 
 for (let i=1;i<20;i++) {
 
     const button = document.createElement('button');
-    button.style.width='50px';
+    button.style.width=button_width+'px';
     button.textContent=i;
 
     if (i==14) {
-        button.style.height='110px';
+        button.style.height=button_height*2+gap;
         button.style.gridRow='span 2';
     } else {
-        button.style.height='50px';
+        button.style.height=button_height;
     }
 
     grid.appendChild(button);
