97d144bba18c53d60655c8a13155e38394ed7c54
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2022-10-21 21:39:50 -0700

    set color scheme for calculator

--- scripts.js
+++ scripts.js
@@ -1,31 +1,35 @@
 document.body.style.display='flex';
 document.body.style.justifyContent='center';
-document.body.style.backgroundColor='blue';
 document.body.style.height='100vh';
 document.body.style.margin=0;
 
 const calculator_container = document.createElement('div');
-//calculator_container.style.height='100vh';
 calculator_container.style.display='flex';
 calculator_container.style.flexDirection='column';
 calculator_container.style.justifyContent='center';
 document.body.appendChild(calculator_container);
 
+let gap=10;
+
 const view = document.createElement('div');
-view.style.backgroundColor = 'green';
+view.style.backgroundColor = 'rgb(0,200,0)';
 view.style.height='100px';
 view.style.fontSize='70px';
 view.style.textAlign='right';
+view.style.border=`${gap}px solid lightgrey`;
+view.style.borderRadius=`${gap}px ${gap}px 0px 0px`;
 
 view.textContent='000000000';
 calculator_container.appendChild(view);
 
 let button_width=80;
 let button_height=60;
-let gap=10;
+
 
 const grid = document.createElement('div');
-grid.style.backgroundColor = 'red';
+grid.style.border=`${gap}px solid lightgrey`;
+grid.style.borderRadius=`0px 0px ${gap}px ${gap}px`;
+grid.style.backgroundColor = 'lightgrey';
 grid.style.grid = `repeat(4,${button_height+'px'}) / repeat(5,${button_width+'px'})`;
 grid.style.rowGap=gap+'px';
 grid.style.columnGap=gap+'px';
